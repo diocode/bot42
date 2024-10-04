0ef9f6c66c9804cfb287227b7d82fd8149b3ca93
juno <you@example.com>
2024-10-04 15:24:03 +0100

    validate_user

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def getAcessToken():
 
 	payload = json.dumps({
 	"grant_type": "client_credentials",
-	"client_id": "",
-	"client_secret": ""
+	"client_id": "u-s4t2af-32b987953f9d75fe9901c49b8cc80a32da7dddf93afe4a2cca080efedf917e30",
+	"client_secret": "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"
 	})
 	headers = {
 	'Content-Type': 'application/json',
@@ -26,15 +26,20 @@ def getAcessToken():
 
 	return access_token
 
-@app.message()
+@app.message("")
 def validateStudent(message, say):
-	if message["text"].lower() == "student":
-		say(f"<@{message['user']}>") #just to make sure the bot is working
-		url = "https://api.intra.42.fr/v2/users/digoncal"
+	if message["text"].lower().startswith("_student"):
+		user = message["text"].split(" ")[1]
+
+		url = "https://api.intra.42.fr/v2/users/" + user
 		payload = {}
 		headers = {'Authorization': 'Bearer ' + getAcessToken()}
 		response = requests.request("GET", url, headers=headers, data=payload)
-		print(response.text)
+		
+		if response.status_code == 200:
+			say("User " + user + " exists")
+		else:
+			say("User " + user + " does not exist")
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
