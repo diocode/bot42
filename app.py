ade931de0d548a919944ad6add9a9503df318b3a
juno <you@example.com>
2024-10-04 15:04:16 +0100

    generate acess_token

--- app.py
+++ app.py
@@ -3,36 +3,38 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from pprint import pprint
 import requests
+import json
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
-'''
-@app.message()
-def sendMessage(message, say, client):
-	pprint(message)
-	if message["text"] == "hello":
-		say(f"Hey there <@{message['user']}>!")
 
-	if message["text"] == ":gandalf:":
-		say(text="There you go again Pedro! :gandalf:", thread_ts=message["ts"])
-	
-		client.reactions_add(
-			channel=message["channel"],
-			name="gandalf",
-			timestamp=message["event_ts"]
-		)
+def getAcessToken():
+	url = "https://api.intra.42.fr/oauth/token"
 
-@app.event("reaction_added")
-def sendReaction(event, say, client):
-	#pprint(event)
-	if event["reaction"] == "gandalf":
-		say(text="There you go again Pedro! :gandalf:", thread_ts=event["item"]["ts"])
-'''	
+	payload = json.dumps({
+	"grant_type": "client_credentials",
+	"client_id": "",
+	"client_secret": ""
+	})
+	headers = {
+	'Content-Type': 'application/json',
+	'Cookie': '_mkra_stck=15e20a8020c702e70007eb1e185a06fb%3A1728048352.5550666'
+	}
+
+	response = requests.request("POST", url, headers=headers, data=payload)
+	response_json = response.json()
+	access_token = response_json.get("access_token")
+
+	return access_token
+
 @app.message()
 def validateStudent(message, say):
-	#pprint(message)
 	if message["text"].lower() == "student":
-		say(f"<@{message['user']}>")
-
+		say(f"<@{message['user']}>") #just to make sure the bot is working
+		url = "https://api.intra.42.fr/v2/users/digoncal"
+		payload = {}
+		headers = {'Authorization': 'Bearer ' + getAcessToken()}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		print(response.text)
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
