bcc07c670c0f6a737fa2896cbe36808fd758ae9c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-09 12:11:58 +0100

    Exception Handling

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import logging
 import os
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import app.slack_bot as slack_bot
+
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
     try:
