6100609bc9a201688713ef3ade81dd2f19c6f216
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-09 11:05:45 +0100

    Setup tooling setup

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 if [ ! -d .venv ]; then
     python3 -m venv .venv
     source .venv/bin/activate
-    pip install -r requirements.txt
+    pip install -e .[dev]
+    pip list
 fi
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
   name="LookinSlack",
   version="0.1.0",
-  description="A short description of the project",
+  description="Get Piscine & Student progress data",
   author="passunca & digoncal",
   packages=find_packages(where="app"),
   package_dir={"": "app"},
   install_requires=[
-    "requests", 
     "setuptools", 
     "slack_bolt",
+    "requests", 
   ],
   extras_require={
     "dev": ["debugpy", "black"],
