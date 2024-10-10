a9c9a89e425231f440feaf4c4c0c715032d94b97
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-10 11:48:47 +0100

    printer.py: added blackhole date

--- app/printer.py
+++ app/printer.py
@@ -1,66 +1,71 @@
+from pprint import pprint
 from .warning import warning_status
 
 def format_student_info(student_data):
-	first_name = student_data["first_name"]
-	last_name = student_data["last_name"]
-	login = student_data["login"]
-	cursus = student_data["cursus_users"][-1]["cursus"]["name"]
-	level = student_data["cursus_users"][-1]["level"]
-	projects = student_data["projects_users"]
-	location = student_data["location"]
-	small_image_url = student_data["image"]["versions"]["small"]
+  first_name = student_data["first_name"]
+  last_name = student_data["last_name"]
+  login = student_data["login"]
+  cursus = student_data["cursus_users"][-1]["cursus"]["name"]
+  level = student_data["cursus_users"][-1]["level"]
+  projects = student_data["projects_users"]
+  location = student_data["location"]
+  blackhole = student_data["cursus_users"][-1]["blackholed_at"]
+  small_image_url = student_data["image"]["versions"]["small"]
 
-	progress_data = {}
+  # pprint("{blackhole}")
 
-	exams = [
-		project
-		for project in student_data["projects_users"]
-		if "Exam" in project["project"]["name"]
-	]
-	exams_str = "\n".join(
-		[
-			f">{p['project']['name']}:  `{p['final_mark'] or 'In Progress'}`"
-			for p in exams
-		]
-	)
+  progress_data = {}
 
-	# Fill progress_data with exam results
-	for exam in exams:
-		progress_data[exam['project']['name']] = exam.get('final_mark', 'In Progress')
+  exams = [
+    project
+    for project in student_data["projects_users"]
+    if "Exam" in project["project"]["name"]
+  ]
+  exams_str = "\n".join(
+    [
+      f">{p['project']['name']}:  `{p['final_mark'] or 'In Progress'}`"
+      for p in exams
+    ]
+  )
 
-	only_projects = [
-		project
-		for project in projects
-		if "Exam" not in project["project"]["name"]
-	]
-	recent_projects = sorted(
-		only_projects, key=lambda x: x["marked_at"] or "", reverse=True
-	)[:10]
-	projects_str = "\n".join(
-		[
-			f">{p['project']['name']}:  `{p['final_mark'] or 'In Progress'}`"
-			for p in recent_projects
-		]
-	)
+  # Fill progress_data with exam results
+  for exam in exams:
+    progress_data[exam['project']['name']] = exam.get('final_mark', 'In Progress')
 
-	# Fill progress_data with project results
-	for project in recent_projects:
-		progress_data[project['project']['name']] = project.get('final_mark', 'In Progress')
+  only_projects = [
+    project
+    for project in projects
+    if "Exam" not in project["project"]["name"]
+  ]
+  recent_projects = sorted(
+    only_projects, key=lambda x: x["marked_at"] or "", reverse=True
+  )[:10]
+  projects_str = "\n".join(
+    [
+      f">{p['project']['name']}:  `{p['final_mark'] or 'In Progress'}`"
+      for p in recent_projects
+    ]
+  )
 
-	return f"""
+  # Fill progress_data with project results
+  for project in recent_projects:
+    progress_data[project['project']['name']] = project.get('final_mark', 'In Progress')
+
+  return f"""
 
 *User:*     `{login}`
 *Name:*    `{first_name} {last_name}`
 *Cluster:* `{location}`
 *Cursus:*   `{cursus}`
 *Level:*     `{level:.2f}`
+*Blackhole:* `{blackhole or 'N/A'}`
 
 
 📂 *Recent Projects :*
 {projects_str}
 
 
-📝 *Exams :* 
+📝 *Exams :*
 {exams_str}
 
 *Warning:* {warning_status(progress_data)}
